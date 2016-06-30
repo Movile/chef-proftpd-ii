e0b729e5a3b928e10237febd85c586bc9f539890
Hugo Cisneiros <hugo.cisneiros@movile.com>
2016-06-30 12:04:41 -0300

    attribute explanations

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,65 +18,132 @@
 # limitations under the License.
 #
 
-# daemon directories and files
+# NOTE: Many of these configuration directives are detailed explained
+# at the ProFTPd's documentation. See:
+# http://www.proftpd.org/docs/
+
+# Configuration directory root. All configuration will be under this
+# directory
 default['proftpd-ii']['conf_dir'] = '/etc/proftpd'
+
+# Log directory root. All logs will be created under this directory.
 default['proftpd-ii']['log_dir'] = '/var/log/proftpd'
+
+# Modules directory root. Modules will be loaded from this directory.
 default['proftpd-ii']['mods_dir'] = '/usr/libexec/proftpd'
+
+# Where the proftpd.score file is located
 default['proftpd-ii']['scoreboard_file'] = '/var/run/proftpd.score'
+
+# Where the PID file for the daemon is located
 default['proftpd-ii']['pid_file'] = '/var/run/proftpd.pid'
 
-# user and group for running the daemon
+# User and group for running the daemon
 default['proftpd-ii']['user'] = 'proftpd'
 default['proftpd-ii']['group'] = 'proftpd'
 
-# home directory and shell for the daemon user
+# Home directory and shell for the daemon user
 default['proftpd-ii']['user_dir'] = '/var/lib/ftp'
 default['proftpd-ii']['user_shell'] = '/sbin/nologin'
 
-# modules definitions
+# What modules will we configure and load?
 default['proftpd-ii']['basic_modules'] = %w()
 default['proftpd-ii']['extra_modules'] = %w()
 
-# banner info
+# When a client connects, how do we name ourselves?
 default['proftpd-ii']['server_name'] = 'ProFTPD server'
+
+# Email from the server admin
 default['proftpd-ii']['server_admin'] = 'ftpmaster@example.com'
+
+# When a client logins, what do we show?
 default['proftpd-ii']['server_indent'] = false
+
+# Don't show welcome message until user has authenticated
 default['proftpd-ii']['defer_welcome'] = true
 
-# connection
+# Number of max instances of proftpd running. Useful for limiting
+# connections to the daemon and preventing DDoS.
 default['proftpd-ii']['max_instances'] = 30
+
+# Default port the daemon will use to listen for connections
 default['proftpd-ii']['port'] = 21
+
+# Port range the server will announce when the client uses passive mode
 default['proftpd-ii']['passive_ports'] = '49152 65534'
+
+# If using the server behind NAT, you can specify the real address here,
+# so the server can report itself to the client correctly.
 default['proftpd-ii']['nat'] = false
+
+# Ident lookups and reverse DNS lookups when the client connects to the
+# server. Disabled by default for performance.
 default['proftpd-ii']['ident_lookups'] = false
 default['proftpd-ii']['reverse_dns'] = false
+
+# How many login errors a client can do before the server disconnects it.
 default['proftpd-ii']['max_login_attempts'] = 3
+
+# Do we allow Server-to-Server transfers (FXP)?
 default['proftpd-ii']['fxp'] = false
 
-# login
+# The authentication module order. ProFTPd will look at each authentication
+# module and try using the defined order. See ProFTPd's documentation for
+# more detailed info. 
 default['proftpd-ii']['auth_order'] = 'mod_auth_pam.c mod_auth_unix.c'
+
+# Do we allow root logins?
 default['proftpd-ii']['root_login'] = false
+
+# Do we present a password prompt after connecting? If not, client has to
+# provide immediately at the connection.
 default['proftpd-ii']['login_password_prompt'] = true
+
+# Limits the connections per client machine (Integer)
 default['proftpd-ii']['max_clients_host'] = false
+
+# Users can only login if they have a valid shell configured. The valid
+# shells list are based on /etc/shells file.
 default['proftpd-ii']['require_valid_shell'] = true
 
-# transfers
+# Timeout on stalled data transfers (seconds)
 default['proftpd-ii']['timeout_stalled'] = 3600
+
+# Connection without transfer timeout (seconds)
 default['proftpd-ii']['timeout_no_transfer'] = 300
+
+# Idle connection timeout (seconds)
 default['proftpd-ii']['timeout_idle'] = 600
+
+# Allow download resume?
 default['proftpd-ii']['retrieve_restart'] = true
+
+# Allow upload resume?
 default['proftpd-ii']['retrieve_store'] = true
 
-# files and directories
+# What is the root directory when a user logins? Defaults to the home
+# directory of the user (besides the adm account). See ProFTPd's
+# documentation for more info.
 default['proftpd-ii']['default_root'] = '~ !adm'
+
+# umask for permissions used when the user uploads new files.
 default['proftpd-ii']['umask'] = '022'
+
+# What options do we append to the list directory command? Defaults to
+# showing hidden files (ls -a)
 default['proftpd-ii']['list_options'] = '-a'
+
+# RFC2228 multiline response mode
 default['proftpd-ii']['rfc2228'] = false
 
-# logging
+# Default log format 
 default['proftpd-ii']['log_format_default'] = '%h %l %u %t \"%r\" %s %b'
+
+# Authentication log format
 default['proftpd-ii']['log_format_auth'] = '%v [%P] %h %t \"%r\" %s'
+
+# Debug Level for the daemon
 default['proftpd-ii']['debug_level'] = 0
 
-# tls
+# What ciphers will TLS use when negotiating with the client
 default['proftpd-ii']['tls_ciphers'] = 'ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:-SSLv2:+EXP'
