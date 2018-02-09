d5432b099c638ea99cf9f376692e2d07d7e5b996
Hugo Cisneiros <hugo.cisneiros@movile.com>
2018-02-09 18:18:49 -0200

    bump version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures proftpd-ii'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version     '>= 11.0' if respond_to?(:chef_version)
-version          '0.5.3'
+version          '0.5.4'
 issues_url       'https://github.com/Movile/chef-proftpd-ii/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/Movile/chef-proftpd-ii' if respond_to?(:source_url)
 
